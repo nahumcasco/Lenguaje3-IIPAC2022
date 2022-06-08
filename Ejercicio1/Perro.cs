ae88a42f94c68fda9424d5f9b5020b46b56fab0d
Nahum Casco <nahumcasco@gmail.com>
2022-06-07 20:55:32 -0600

    crear clase perro
    crear clase perro

---
+++ Ejercicio1/Perro.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    public class Perro
+    {
+        //Atributos
+        private string nombre;
+        private string raza;
+        private decimal altura;
+        
+        //Propiedades
+        public string Nombre { 
+            get { return nombre; } 
+            set { nombre = value; } 
+        }
+
+        public string Raza
+        {
+            get { return raza; }
+            set { raza = value; }
+        }
+
+        public decimal Altura
+        {
+            get { return altura; }
+            set { altura = value; }
+        }
+
+        //Otra manera de crear Propiedades
+        //public string Nombre { get; set; }
+        //public string Raza { get; set; }
+        //public decimal Altura { get; set; }
+
+
+        //Constructor
+        public Perro()
+        { 
+        }
+
+        public Perro(string nombre, string raza, decimal altura)
+        {
+            Nombre = nombre;
+            Raza = raza;
+            Altura = altura;
+        }
+
+        public Perro(string nombre)
+        {
+            Nombre = nombre;
+        }
+
+        //Metodos
+        public decimal DevuelveAltura()
+        {
+            return Altura;
+        }
+
+        public string Comer(string comida)
+        {
+            return Nombre + " comerá " + comida;
+        }
+
+
+    }
+}
